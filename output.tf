a0703da501ce7692a605c2d05decc10cf855a4e7
Hnin Wut Yee <hwutyee@gmail.com>
2024-01-27 10:54:16 +0800

    commit sns terraform

---
+++ output.tf
@@ -0,0 +1,5 @@
+output "SNSArn" {
+description = "SNS alreting topics"
+value = aws_sns_topic.snstopic.arn
+
+}
---
+++ sns.tf
@@ -0,0 +1,14 @@
+resource "aws_sns_topic" "snstopic" {
+name = "sns-demo-topic"
+display_name = "sns-demo-topic"
+tags = {
+"Name" = "sns-demo-topic-alerts"
+}
+
+}
+
+resource "aws_sns_topic_subscription" "snstopicsubscription" {
+topic_arn = aws_sns_topic.snstopic_arn
+protocol = "email"
+endpoint = "hwutyee@gmail.com"
+}
