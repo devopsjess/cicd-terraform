9422c7d8fe8005b265603beea5dbe3d20003a18c
Hnin Wut Yee <hwutyee@gmail.com>
2024-01-27 11:56:26 +0800

    fistcicdpipeline

--- sns.tf
+++ sns.tf
@@ -7,8 +7,8 @@ tags = {
 
 }
 
-resource "aws_sns_topic_subscription" "snstopicsubscription" {
-topic_arn = aws_sns_topic.snstopic_arn
-protocol = "email"
-endpoint = "hwutyee@gmail.com"
-}
+#resource "aws_sns_topic_subscription" "snstopicsubscription" {
+#topic_arn = aws_sns_topic.snstopic_arn
+#protocol = "email"
+#endpoint = "hwutyee@gmail.com"
+#}
